6f0d76da74695573883b7aecb36ebf3ed893a833
Nicolas VERGNAC <nvergnac@e1r5p4.42.fr>
2018-05-29 17:38:46 +0200

    ajout modif_carry

--- vm/srcs/ft_utilities.c
+++ vm/srcs/ft_utilities.c
@@ -6,11 +6,16 @@
 /*   By: nvergnac <nvergnac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 16:49:52 by nvergnac          #+#    #+#             */
-/*   Updated: 2018/05/29 16:50:31 by nvergnac         ###   ########.fr       */
+/*   Updated: 2018/05/29 17:38:18 by nvergnac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+void			ft_modif_carry(t_proc *proc, int carry_value)
+{
+	proc->carry = carry_value;
+}
 
 void			ft_uint_to_ptr(t_info *info, unsigned int ptr, int size, unsigned int value)
 {
@@ -25,13 +30,13 @@ void			ft_uint_to_ptr(t_info *info, unsigned int ptr, int size, unsigned int val
 		ft_putstr("ptr[i] :\t");
 		ft_putnbr(info->board[ptr]);
 		ft_putstr("\n");
-		ptr = (ptr + 1) % 4096;
+		ptr = (ptr + 1) % MEM_SIZE;
 		i++;
 		k--;
 	}
 }
 
-unsigned int	ft_ptr_to_uint(unsigned char *ptr, int size)
+unsigned int	ft_ptr_to_uint(t_info *info, unsigned int ptr, int size)
 {
 	int				i;
 	unsigned int	result;
@@ -40,8 +45,9 @@ unsigned int	ft_ptr_to_uint(unsigned char *ptr, int size)
 	result = 0;
 	while (i < size)
 	{
-		result = result * 256 + ptr[i];
+		result = result * 256 + info->board[ptr];
 		i++;
+		ptr = (ptr + i) % MEM_SIZE;
 	}
 	return (result);
 }
