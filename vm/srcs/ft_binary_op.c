5eebfe027b1d34316a32292ee90a461d01749dfd
Pablo CLEMENT <pclement@e1r11p6.42.fr>
2018-06-01 14:42:14 +0200

    before merge

--- vm/srcs/ft_binary_op.c
+++ vm/srcs/ft_binary_op.c
@@ -6,7 +6,7 @@
 /*   By: pclement <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 20:14:06 by pclement          #+#    #+#             */
-/*   Updated: 2018/05/29 20:14:16 by pclement         ###   ########.fr       */
+/*   Updated: 2018/05/30 17:15:11 by pclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,28 @@ int	ft_and(t_info *info, t_proc *proc)
 	unsigned int	reg_idx;
 	unsigned int	val;
 
-	ft_putstr("Je commence AND\n");
+	ft_putnbr(proc->reg[0]);
+	ft_putstr("____________________________________________________________________________________________________________________________________________Je commence AND\n");
 	param1 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2) % MEM_SIZE, proc->loaded_op.param_size[0]);
 	param2 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2 + proc->loaded_op.param_size[0]) % MEM_SIZE, proc->loaded_op.param_size[1]);
 	
+	ft_putstr("param1\n");
+	ft_putnbr(param1);
+	ft_putstr("param2\n");
+	ft_putnbr(param2);
 	reg_idx = ft_ptr_to_uint(info, (2 + proc->pc - proc->op_size + proc->loaded_op.param_size[0] + proc->loaded_op.param_size[1]) % MEM_SIZE, proc->loaded_op.param_size[2]);
 	if (reg_idx < 1 || reg_idx > 16)
 		return (0);
-	if ((val = param1 & param2) == 0)
+	val = param1 & param2;
+	if (val == 0)
 		proc->carry = 1;
 	else
 		proc->carry = 0;
 	proc->reg[reg_idx - 1] = val;
+	ft_putnbr(val);
+	ft_putstr("\n");
+	ft_putnbr(proc->reg[reg_idx - 1]);
+	ft_putstr("\n");
 	ft_putstr("I'm ANDED\n");
 	return (0);
 }
@@ -42,18 +52,24 @@ int	ft_or(t_info *info, t_proc *proc)
 	unsigned int	reg_idx;
 	unsigned int	val;
 
-	ft_putstr("Je commence OR\n");
+	ft_putnbr(proc->reg[0]);
+	ft_putstr("____________________________________________________________________________________________________________________________________________Je commence OR\n");
 	param1 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2) % MEM_SIZE, proc->loaded_op.param_size[0]);
 	param2 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2 + proc->loaded_op.param_size[0]) % MEM_SIZE, proc->loaded_op.param_size[1]);
 	
 	reg_idx = ft_ptr_to_uint(info, (2 + proc->pc - proc->op_size + proc->loaded_op.param_size[0] + proc->loaded_op.param_size[1]) % MEM_SIZE, proc->loaded_op.param_size[2]);
 	if (reg_idx < 1 || reg_idx > 16)
 		return (0);
-	if ((val = param1 | param2) == 0)
+	val = param1 | param2;
+	if (val  == 0)
 		proc->carry = 1;
 	else
 		proc->carry = 0;
 	proc->reg[reg_idx - 1] = val;
+	ft_putnbr(val);
+	ft_putstr("\n");
+	ft_putnbr(proc->reg[reg_idx - 1]);
+	ft_putstr("\n");
 	ft_putstr("I'm ORED\n");
 	return (0);
 }
@@ -65,7 +81,7 @@ int	ft_xor(t_info *info, t_proc *proc)
 	unsigned int	reg_idx;
 	unsigned int	val;
 
-	ft_putstr("Je commence XOR\n");
+	ft_putstr("____________________________________________________________________________________________________________________________________________Je commence XOR\n");
 	param1 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2) % MEM_SIZE, proc->loaded_op.param_size[0]);
 	param2 = ft_ptr_to_uint(info, (proc->pc - proc->op_size + 2 + proc->loaded_op.param_size[0]) % MEM_SIZE, proc->loaded_op.param_size[1]);
 	
@@ -77,6 +93,10 @@ int	ft_xor(t_info *info, t_proc *proc)
 	else
 		proc->carry = 0;
 	proc->reg[reg_idx - 1] = val;
+	ft_putnbr(val);
+	ft_putstr("\n");
+	ft_putnbr(proc->reg[reg_idx - 1]);
+	ft_putstr("\n");
 	ft_putstr("I'm XORED\n");
 	return (0);
 }
