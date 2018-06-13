305750c7ebf84ac66745167ee1564242ff8790de
Nicolas VERGNAC <nvergnac@e1r9p3.42.fr>
2018-06-13 14:37:58 +0200

    merge suppression putstr

--- vm/srcs/ft_basic_op.c
+++ vm/srcs/ft_basic_op.c
@@ -6,7 +6,7 @@
 /*   By: nvergnac <nvergnac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 20:13:38 by nvergnac          #+#    #+#             */
-/*   Updated: 2018/06/06 16:29:35 by nvergnac         ###   ########.fr       */
+/*   Updated: 2018/06/11 20:02:07 by nvergnac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int	ft_live_write(t_info *info, t_proc *proc, int live_int)
 	return (0);
 }
 
-
 int	ft_live(t_info *info, t_proc *proc)
 {
 	int i;
@@ -44,7 +43,7 @@ int	ft_live(t_info *info, t_proc *proc)
 			ft_live_write(info, proc, live_int);
 			info->players_info[i].live++;
 			proc->alive = 1;
-			info->last_player_alive = info->players_info[i].number - 1;
+			info->last_player_alive = info->players_info[i].number;
 		}
 		i++;
 	}
@@ -57,30 +56,18 @@ int	ft_load(t_info *info, t_proc *proc)
 	int reg_idx;
 
 	val = 0;
-	ft_putstr("Je commence LOAD\n");
 	reg_idx = ft_ptr_to_uint(info, proc->pc + P_SIZE[0], P_SIZE[1]);
 	if (info->board[ft_mod_memsize(proc->pc - 1)] == 144)
 		val = ft_ptr_to_uint(info, proc->pc, P_SIZE[0]);
 	if (info->board[ft_mod_memsize(proc->pc - 1)] == 208)
 	{
-		ft_putstr("TEST :\t");
-		ft_putnbr(ft_idx_mod(proc, ft_ptr_to_uint(info, proc->pc, P_SIZE[0])));
-		ft_putstr("\n");
 		val = ft_ptr_to_uint(info, proc->pc - 2 + ft_idx_mod(proc,
 				ft_ptr_to_uint(info, proc->pc, P_SIZE[0])),
 				proc->loaded_op.dir_size);
 	}
-	ft_putstr("val :\t");
-	ft_putnbr(val);
-	ft_putstr("\n");
-	ft_putstr("reg_idx :\t");
-	ft_putnbr(reg_idx);
-	ft_putstr("\n");
 	if (reg_idx < 1 || reg_idx > 16)
 		return (0);
 	proc->reg[reg_idx - 1] = val;
-	ft_putstr("\n");
-	ft_putstr("I'm Loaded\n");
 	(val == 0) ? ft_modif_carry(proc, 1) : ft_modif_carry(proc, 0);
 	return (0);
 }
@@ -90,8 +77,7 @@ int	ft_store(t_info *info, t_proc *proc)
 	int val;
 	int reg_src_idx;
 	int reg_dest_idx;
-	
-	ft_putstr("Je commence STORE\n");
+
 	val = 0;
 	reg_dest_idx = 0;
 	reg_src_idx = info->board[proc->pc];
@@ -102,24 +88,14 @@ int	ft_store(t_info *info, t_proc *proc)
 		reg_dest_idx = info->board[ft_mod_memsize(proc->pc + P_SIZE[0])];
 		if (reg_dest_idx <= 0 || reg_dest_idx > 16)
 			return (0);
-		ft_putstr("reg_dest_idx :\t");
-		ft_putnbr(reg_dest_idx);
 		proc->reg[reg_dest_idx - 1] = proc->reg[reg_src_idx - 1];
 	}
 	if (info->board[ft_mod_memsize(proc->pc - 1)] == 112)
 	{
 		val = ft_ptr_to_uint(info, proc->pc + P_SIZE[0], P_SIZE[1]);
-		ft_putstr("val :\t");
-		ft_putnbr(val);
-		ft_putstr("\n");
 		ft_uint_to_ptr(info, proc->pc - 2 + ft_idx_mod(proc, val),
 				REG_SIZE, proc->reg[reg_src_idx - 1]);
 	}
-	ft_putstr("reg_src_idx :\t");
-	ft_putnbr(reg_src_idx);
-	ft_putstr("\t reg_src_idx_value :\t");
-	ft_putnbr(proc->reg[reg_src_idx - 1]);
-	ft_putstr("\nI'm Storeded\n");
 	return (0);
 }
 
@@ -130,8 +106,6 @@ int		ft_aff(t_info *info, t_proc *proc)
 	reg_idx = info->board[proc->pc];
 	if (reg_idx < 1 || reg_idx > 16)
 		return (0);
-	ft_putstr("\n________________________________JE COMMENCE AFF\n");
 	ft_putchar(proc->reg[reg_idx - 1]);
-	ft_putstr("\n________________________________Jai AFF\n");
 	return (0);
 }
--- vm/srcs/ft_jump_n_fork.c
+++ vm/srcs/ft_jump_n_fork.c
@@ -6,7 +6,7 @@
 /*   By: nvergnac <nvergnac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 20:08:45 by nvergnac          #+#    #+#             */
-/*   Updated: 2018/06/06 14:22:41 by nvergnac         ###   ########.fr       */
+/*   Updated: 2018/06/11 20:02:09 by nvergnac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,7 @@ void	ft_kill_proc(t_info *info)
 	while (proc_tmp)
 	{
 		if (proc_tmp->alive <= 0)
-		{
-			ft_putstr("KIIIIIIIIILLLLLLLLLLLLLLLLLLLLLLLLLL PROOOOOOOOCCCCCCCCCCCCCCCCCCCC\n");
 			proc_tmp->alive = -1;
-		}
 		else
 			proc_tmp->alive = 0;
 		proc_tmp = proc_tmp->next;
@@ -70,14 +67,9 @@ int	ft_fork(t_info *info, t_proc *proc)
 {
 	int	fork_val;
 
-	ft_putstr("Je commence FORK\n");
 	fork_val = 0;
 	fork_val = ft_ptr_to_uint(info, proc->pc, P_SIZE[0]);
-	ft_putstr("fork_val :\t");
-	ft_putnbr(ft_idx_mod(proc, fork_val));
-	ft_putstr("\n");
 	ft_add_new_proc(info, proc, ft_idx_mod(proc, fork_val));
-	ft_putstr("J'ai FORKED\n");
 	return (0);
 }
 
@@ -85,14 +77,9 @@ int	ft_long_fork(t_info *info, t_proc *proc)
 {
 	int	fork_val;
 
-	ft_putstr("Je commence LONG FORK\n");
 	fork_val = 0;
 	fork_val = ft_ptr_to_uint(info, proc->pc, P_SIZE[0]);
-	ft_putstr("fork_val :\t");
-	ft_putnbr(fork_val);
-	ft_putstr("\n");
 	ft_add_new_proc(info, proc, fork_val);
-	ft_putstr("J'ai LONG FORKED\n");
 	return (0);
 }
 
@@ -100,13 +87,11 @@ int	ft_zjump(t_info *info, t_proc *proc)
 {
 	int jmp_val;
 
-	ft_putstr("Je commence JUMP_AROUND           JUMP JUMP JUMP\n");
 	jmp_val = 0;
 	if (proc->carry != 1)
 		return (0);
 	jmp_val = ft_ptr_to_uint(info, proc->pc, P_SIZE[0]);
 	proc->pc = ft_mod_memsize(proc->pc - 1 - proc->op_size + jmp_val);
-	ft_putstr("J'ai JUMPED\n");
 	return (0);
 }
 
