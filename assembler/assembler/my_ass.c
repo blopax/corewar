451791761fdeecbb99e616cef6bd0bd314b58abc
Anthony TOURNERIE <atourner@e1r8p3.42.fr>
2018-06-15 17:40:50 +0200

    merge

--- assembler/assembler/my_ass.c
+++ assembler/assembler/my_ass.c
@@ -6,7 +6,7 @@
 /*   By: atourner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 10:58:30 by atourner          #+#    #+#             */
-/*   Updated: 2018/05/07 17:55:19 by atourner         ###   ########.fr       */
+/*   Updated: 2018/06/15 17:38:17 by atourner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,19 @@ int			main(int ac, char **av)
 {
 	t_print		*file;
 
-	if (ac == 1)
-		ft_printf("Usage : The assembly need at least one file to be used\n");
+	file = NULL;
+	if (ac != 2)
+		ft_printf("Usage : The assembly need only one file to be used\n");
 	else
 	{
-		while (++av && *av)
+		if (ft_valid_name(av[1]) &&
+				(file = ft_val_asm(open(av[1], O_RDONLY))) && file->str)
+			ft_print_file(file, av[1]);
+		else
 		{
-			if (ft_valid_name(av[0]) &&
-					(file = ft_val_asm(open(av[0], O_RDONLY))) && file->str)
-				ft_print_file(file, av[0]);
-			else
-			{
-				free(file);
-				ft_printf("An error has occured for the compilation \
+			free(file);
+			ft_printf("An error has occured for the compilation \
 of your file %s\n", av[0]);
-			}
 		}
 	}
 	return (0);
